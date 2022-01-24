f3065b042c9ffff41d33d1d735e6e4644e40fa5b
alekseevayana <alekseevayana@bk.ru>
2022-01-24 15:00:38 -0800

    browserrouter basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/meals_planner">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
