27872881e66ad950655d4946ad885a1a890bdb23
alekseevayana <alekseevayana@bk.ru>
2022-01-24 17:20:56 -0800

    change base url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/meals_planner">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
