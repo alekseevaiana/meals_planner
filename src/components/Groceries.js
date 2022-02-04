758540a6aff24f810fbe8eb1ce4c64d370e24007
alekseevayana <alekseevayana@bk.ru>
2022-02-04 10:47:49 -0800

    create new component for groceries

---
+++ src/components/Groceries.js
@@ -0,0 +1,15 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Groceries() {
+  return (
+    <Box>
+      <Typography variant="h6" color="primary.dark">
+        Groceries
+      </Typography>
+      {/* 
+      - Render checklist
+      - Add new ingridient with Autocomplit with parametrs: name, quaontity, category, where to buy
+      */}
+    </Box>
+  );
+}
