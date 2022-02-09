0b8b96fe2ebcad3caf2f6bc0bcf73f1bbf6bde0c
alekseevayana <alekseevayana@bk.ru>
2022-02-09 10:57:25 -0800

    separate input for adding ingridient

--- src/components/AddIngridient.js
+++ src/components/AddIngridient.js
@@ -1,7 +1,6 @@
-import { useState } from "react";
-import { TextField, Box, Autocomplete } from "@mui/material";
+import { Box } from "@mui/material";
 import IngridientsList from "./IngridientsList";
-import AddIcon from "@mui/icons-material/Add";
+import InputAddIngridient from "./InputAddIngridient";
 
 export default function AddIngridient({
   ingridients,
@@ -9,57 +8,13 @@ export default function AddIngridient({
   handleDelete,
   allIngridients,
 }) {
-  const [name, setName] = useState("");
-
-  const handleAdd = () => {
-    const updated = [...ingridients];
-    console.log("name:", name);
-    if (!updated.includes(name)) {
-      updated.push(name);
-    }
-    setName("");
-    onChange(updated);
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      console.log("enter");
-      handleAdd();
-    }
-  };
-
   return (
     <Box>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Autocomplete
-          sx={{ width: "100%" }}
-          id="free-solo-demo"
-          freeSolo
-          options={allIngridients.map((option) => option)}
-          onKeyPress={handleKeyPress}
-          onChange={(event, newValue) => {
-            setName(newValue);
-          }}
-          value={name}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Add ingridient"
-              variant="outlined"
-              onChange={(event) => setName(event.target.value)}
-            />
-          )}
-        />
-        <Box sx={{ p: 1 }}>
-          <AddIcon
-            aria-label="toggle ingridient visibility"
-            onClick={handleAdd}
-            edge="end"
-            sx={{ ml: "auto" }}
-          />
-        </Box>
-      </Box>
+      <InputAddIngridient
+        savedIngridients={ingridients}
+        allIngridients={allIngridients}
+        onChange={onChange}
+      />
       <Box sx={{ mb: 2 }}>
         <IngridientsList
           ingridients={ingridients}
---
+++ src/components/InputAddIngridient.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { TextField, Box, Autocomplete } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+export default function InputAddIngridient({
+  allIngridients,
+  savedIngridients,
+  onChange,
+}) {
+  const [name, setName] = useState("");
+
+  const handleAdd = () => {
+    // here is all ingridients that comes with meal
+    // in case groceries or pantry should be the same
+    const updated = [...savedIngridients];
+    // if there is NO such ingridient come with input name -> push it to the list
+    if (!updated.includes(name)) {
+      updated.push(name);
+    }
+    setName("");
+    // call func with new ingridients list
+    onChange(updated);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleAdd();
+    }
+  };
+  return (
+    <>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Autocomplete
+          sx={{ width: "100%" }}
+          id="free-solo-demo"
+          freeSolo
+          options={allIngridients.map((option) => option)}
+          onKeyPress={handleKeyPress}
+          onChange={(event, newValue) => {
+            setName(newValue);
+          }}
+          value={name}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Add ingridient"
+              variant="outlined"
+              onChange={(event) => setName(event.target.value)}
+            />
+          )}
+        />
+        <Box sx={{ p: 1 }}>
+          <AddIcon
+            aria-label="add ingridient"
+            // could be added to ingridients in meal or another list
+            onClick={handleAdd}
+            edge="end"
+            sx={{ ml: "auto" }}
+          />
+        </Box>
+      </Box>
+    </>
+  );
+}
