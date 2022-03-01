6221da46b66e90c2f8c3186fc29baf0aadaa00f6
alekseevayana <alekseevayana@bk.ru>
2022-03-01 15:38:13 -0800

    add local storage

--- src/components/Groceries.js
+++ src/components/Groceries.js
@@ -1,10 +1,17 @@
 import { Box, Typography } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CheckboxList from "./CheckboxList";
 import InputAddIngridient from "./InputAddIngridient";
+import { getFromStorage } from "../helper";
 
 export default function Groceries({ allIngridients }) {
-  const [groceries, setGroceries] = useState([]);
+  const [groceries, setGroceries] = useState(() =>
+    getFromStorage("groceries", [])
+  );
+
+  useEffect(() => {
+    localStorage.setItem("groceries", JSON.stringify(groceries));
+  }, [groceries]);
 
   const updateIngridients = (updated) => {
     setGroceries(updated);
